2fe57255d76c300828251ded4a3c8ce453383909
Gerkkk <danpol2005@gmail.com>
2025-01-19 22:58:22 +0300

    fix

---
+++ SightSeeStalker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SightSeeStalker
+//
+//  Created by Danya Polyakov on 19.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
